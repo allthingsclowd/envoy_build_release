6cb812b55f9a165eb2a9bec864c3833ee91774d7
Graham Land <graham@allthingscloud.eu>
2018-09-30 15:44:51 +0100

    hmm

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -x
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7.0 main"
+
 sudo apt-get update
-sudo apt-get install -y clang-format-7
+sudo apt-get install clang-format-7
