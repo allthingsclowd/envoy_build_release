30b45e7d8b3353dd9ac8cde1aee50747d508fe8a
Graham Land <graham@allthingscloud.eu>
2018-09-30 15:37:24 +0100

    updated clang? library name

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -4,4 +4,4 @@ set -x
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo apt-add-repository "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7.0 main"
 sudo apt-get update
-sudo apt-get install -y clang-7.0
+sudo apt-get install -y clang-format-7
