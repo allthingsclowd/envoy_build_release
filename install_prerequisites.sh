e4b7f328e204019ed0c9b7a734c756aef60fce98
Graham Land <graham@allthingscloud.eu>
2018-09-30 15:55:16 +0100

    t2

--- install_prerequisites.sh
+++ install_prerequisites.sh
@@ -2,5 +2,10 @@
 
 set -x
 
-sudo apt-get update
-sudo apt-get install clang-format-7
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-add-repository 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main'
+sudo apt-get -qq update
+sudo apt-get -qq --force-yes install clang-7.0 clang-modernize-7.0 # clang-format-3.5
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7.5 1
+sudo apt-get -qq --force-yes install clang-format-7
+
